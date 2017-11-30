7b7699d9c7316d1be624d2ea25e4f7e52e356f4e
Barnabas Makonda <barnabasmakonda@gmail.com>
2017-11-30 12:05:01 +0300

    try to clean index.js as much as possible

--- bin/event-transaction.js
+++ bin/event-transaction.js
@@ -1,5 +1,9 @@
 const { pool } = require('./connection');
 const moment = require('moment');
+const { generateCodes, generateCode } = require('dhis2-uid');
+const { convertCsvToJson } = require('./configManager');
+const _ = require('lodash');
+const { mapSeries } = require('async');
 
 let event_entered = 0;
 const eventTranscation = async (psiValues, tedValues) => {
@@ -54,4 +58,58 @@ const postData = (psiValues, tedValues, callBackFn) => {
       callBackFn(e, e.stack);
     });
 };
-module.exports = { postData };
+
+const programstageinstanceValueKeys = [
+  'programinstanceid',
+  'programstageid',
+  'attributeoptioncomboid',
+  'storedby',
+  'completedby',
+  'organisationunitid',
+  'status',
+  'completeddate',
+  'executiondate'
+];
+
+let noOfEventEntered = 0;
+let noOfEventErrored = 0;
+const eventObject = events => {
+  mapSeries(
+    events,
+    (csvEvent, callBackFn) => {
+      let pEvent = {
+        pstageValues: [],
+        teiValues: []
+      };
+      _.forEach(programstageinstanceValueKeys, pkey => {
+        pEvent.pstageValues.push(csvEvent[pkey]);
+      });
+      pEvent.pstageValues.push(...[generateCode()]);
+      Object.keys(csvEvent).forEach((key, indx) => {
+        if (!csvEvent[key] || csvEvent[key] == '') {
+          return;
+        }
+        if (programstageinstanceValueKeys.indexOf(key) == -1) {
+          let teiEntry = [];
+          teiEntry.push(...[key, csvEvent[key], false, csvEvent['storedby']]);
+          pEvent.teiValues.push(teiEntry);
+        }
+      });
+
+      // Here pass the value to the event-transaction
+      const eventEntered = postData(
+        pEvent.pstageValues,
+        pEvent.teiValues,
+        callBackFn
+      );
+    },
+    (err, results) => {
+      console.info('=====Summary=======');
+      console.info('Number of event Successfully entered: ', results.length);
+      console.info('Number of events errored: ', err);
+      console.info('=========THE END========');
+    }
+  );
+};
+
+convertCsvToJson(eventObject);
